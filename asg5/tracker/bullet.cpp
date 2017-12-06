9eb91ec92e524ac9587c974c27886ac6ec954008
ctrull-clemson <ctrull@g.clemson.edu>
2017-12-05 19:35:49 -0500

    treat is now exploding. doesnt look good but is there. need to move it to free list after exploding for now

--- asg5/tracker/bullet.cpp
+++ asg5/tracker/bullet.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include <cmath>
+#include <functional>
 #include "bullet.h"
 
 void Bullet::update(Uint32 ticks) {
   Vector2f pos = getPosition();
-  if(!tooFar)
+
+  if(tooFar)
+  {
+    explode();
+    explosion->update(ticks);
+    if(explosion->chunkCount() == 0)
+    {
+      delete explosion;
+      explosion = NULL;
+    }
+    tooFar = false;
+  }
+  else if(std::abs(getVelocityX()) < 15)
+  {
+    tooFar = true;
+  }
+  else
   {
     Sprite::update(ticks);
     setVelocityY(.95 * getVelocityY());
@@ -12,4 +29,21 @@ void Bullet::update(Uint32 ticks) {
     distance += ( hypot(getX()-pos[0], getY()-pos[1]) );
     if (distance > maxDistance) tooFar = true;
   }
+
 }
+
+void Bullet::draw() const {
+  if(!explosion) Sprite::draw();
+  else
+  {
+    explosion->draw();
+  }
+}
+
+/*
+void Sprite::draw() const {
+  if(getScale() < SCALE_EPSILON) return;
+  if ( explosion ) explosion->draw();
+  else image->draw(getX(), getY(), getScale());
+}
+*/
--- asg5/tracker/bullet.h
+++ asg5/tracker/bullet.h
@@ -1,17 +1,21 @@
 #include <iostream>
 #include "sprite.h"
 #include "gamedata.h"
+#include "explodingSprite.h"
 
 class Bullet : public Sprite {
 public:
   explicit Bullet(const string& name) :
-    Sprite(name), 
-    distance(0), 
-    maxDistance(Gamedata::getInstance().getXmlInt(name+"/distance")), 
-    tooFar(false) 
+    Sprite(name),
+    distance(0),
+    maxDistance(Gamedata::getInstance().getXmlInt(name+"/distance")),
+    tooFar(false),
+    explosion()
   { }
   virtual void update(Uint32 ticks);
+  virtual void draw() const;
   bool goneTooFar() const { return tooFar; }
+  void explode() { if ( !explosion ) explosion = new ExplodingSprite(*this); };
   void reset() {
     tooFar = false;
     distance = 0;
@@ -21,4 +25,5 @@ private:
   float distance;
   float maxDistance;
   bool tooFar;
+  ExplodingSprite * explosion;
 };
